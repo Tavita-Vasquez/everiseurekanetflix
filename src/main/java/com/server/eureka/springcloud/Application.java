7007cc963f918c14ce54dd65e7cbc289866baa91
Tavita-Vasquez <everis@LIM-HT9WM13.usersad.everis.int>
2020-08-03 18:37:46 -0500

    UPDATE eureka server

--- src/main/java/com/server/eureka/springcloud/Application.java
+++ src/main/java/com/server/eureka/springcloud/Application.java
@@ -1,12 +1,11 @@
 package com.server.eureka.springcloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
